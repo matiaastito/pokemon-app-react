59c0a0a6cc6ccbd419234bb0936fd0dbf1502c22
Matias <matias.tito27@gmail.com>
2022-07-29 13:43:39 -0300

    Update components II

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,22 @@
 import { useState } from "react";
 
-function Header (props){
+const Header = (props) => 
+{
+    //object destructuring
+
+    const {text, text2} = props
 
     const [showSubtitle, setShowSubtitle] = useState (true)
     const [subtitle, setSubtitle] = useState ('Esto es un subtitulooo')
 
+    const handleClick = (e) => {
+        setShowSubtitle(!showSubtitle)
+    }
+
     return (
         <div>
             <h1>{props.text} {props.text2}</h1>
+            <button onClick={handleClick}>Mostrar Subitulos</button>
             {showSubtitle && <h2>{subtitle}</h2>}
         </div>
     );
